11990fe27b6d090601d02ba435e030175a3aaaed
Jack C <jace3373@colorado.edu>
2020-08-30 21:10:44 -0700

    first commit

---
+++ catkin_ws/devel/share/beginner_tutorials/cmake/beginner_tutorials-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(beginner_tutorials_MESSAGE_FILES "/home/jackcenter/Documents/ROS_tutorials/catkin_ws/src/beginner_tutorials/msg/students.msg")
+set(beginner_tutorials_SERVICE_FILES "/home/jackcenter/Documents/ROS_tutorials/catkin_ws/src/beginner_tutorials/srv/AddTwoInts.srv")
