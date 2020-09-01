dbd027a18c82205a2e93d8880f502df70a60c395
Jack C <jace3373@colorado.edu>
2020-09-01 16:00:24 -0700

    added data files, created histogram file, and updated git ignore to inlcude python and pycharm ide

---
+++ histogram_generator.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+
+def main():
+    pub_sub_data_filename = "/home/jackcenter/Desktop/pub_sub_times.txt"
+    srv_client_data_filename =
+
+
+if __name__ == "__main__":
+    main()
